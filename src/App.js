dccf605706fe815513c7bd86588beea9358de006
Nariman Huseynov <hnariman@gmail.com>
2023-02-12 22:12:29 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import QRCode from "react-qr-code";
+import { useForm } from 'react-hook-form';
+import { Fragment, useState } from "react";
 
-function App() {
+const formFields = ["first", "last", "email", "twitter", "website", "mobile"];
+
+const App = () => {
+  // TODO: implement adding custom fields
+  // const [fields, setFields] = useState(formFields);
+  const [data, setData] = useState({});
+  const { register, handleSubmit } = useForm();
+  // TODO: implement to generate some link and place it in QR instead
+  const onSubmit = data => {
+    setData(data);
+    console.log(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className="container">
+      <h1>Please enter you data</h1>
+      <form className="contact-form" onSubmit={handleSubmit(onSubmit)}>
+        {formFields.map(item => (
+          <Fragment key={item}>
+            <label>{item} </label>
+            <input defaultValue=" - " {...register(item)} />
+          </Fragment>
+        ))}
+        <input type="submit" />
+      </form>
+      <QRCode
+        title="some title"
+        value={JSON.stringify(data)}
+        bgColor="black"
+        fgColor="white"
+        level="H"
+        size={200}
+      />
+    </section>
+  )
 }
 
 export default App;
