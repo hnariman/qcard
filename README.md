13c0926695d998f644765f7f633bfb8fb6560a1e
Nariman Huseynov <hnariman@gmail.com>
2023-02-13 00:23:40 +0400

    updated result and improved keys

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # Sample application for QR code generation
 
+Please check deployed version on github pages to see it in action:
+<https://hnariman.github.io/qcard>
+
 ## Scripts
 
 ### `npm start`
 
 Runs the app in the development mode.\
 Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+### gh pages deployment instructions
+
+<https://github.com/gitname/react-gh-pages>
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import QRCode from "react-qr-code";
 import { useForm } from 'react-hook-form';
 import { Fragment, useState } from "react";
+import { useId } from "react";
 
 const formFields = ["first", "last", "email", "twitter", "website", "mobile"];
 
 const App = () => {
+  const uuid = useId();
   // TODO: implement adding custom fields
   // const [fields, setFields] = useState(formFields);
   const [data, setData] = useState({});
@@ -19,13 +21,15 @@ const App = () => {
       <h1>Please enter you data</h1>
       <form className="contact-form" onSubmit={handleSubmit(onSubmit)}>
         {formFields.map(item => (
-          <Fragment key={item}>
+          <Fragment key={uuid + item}>
             <label>{item} </label>
             <input defaultValue=" - " {...register(item)} />
           </Fragment>
         ))}
         <input type="submit" />
       </form>
+      <hr />
+      <h2>QR to share with friends:</h2>
       <QRCode
         title="some title"
         value={JSON.stringify(data)}
@@ -34,6 +38,13 @@ const App = () => {
         level="H"
         size={200}
       />
+      <hr />
+      <h2>Result:</h2>
+      <dd className="user-card">
+        {Object.entries(data).map(([key, value], index) => (
+          <dl key={uuid + index}>{key + " : " + value}</dl>
+        ))}
+      </dd>
     </section>
   )
 }
